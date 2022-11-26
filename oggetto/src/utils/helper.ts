dc60573c97d4f81d185a4eed23ff0b8cab49e0b7
Katrin Chercas <katerinashepeneva@yandex.ru>
2022-11-26 12:36:23 +0300

    feat: create func rank calc

--- oggetto/src/utils/helper.ts
+++ oggetto/src/utils/helper.ts
@@ -1,6 +1,15 @@
-import { rankName } from '../content/RankRule.json'
+import { rankName } from "../content/RankRule.json";
 
 export const calcNameRank = (value: number) => {
-    const t = rankName.filter((item) => value <= item.max)
-    return (t.length ? t[0].name : 'Да кто ты ?!')
-}
+  const unknow = {
+    name: "Да кто ты ?!",
+    max: 0,
+    lobby_title: {
+      hello: "Эй ты, у тебя ошибочный ранг",
+      tasks: "Тебе здесь не рады",
+      power: "ERRORRR ERROORRR",
+    },
+  };
+  const t = rankName.filter((item) => value <= item.max);
+  return t.length ? t[0] : unknow;
+};
