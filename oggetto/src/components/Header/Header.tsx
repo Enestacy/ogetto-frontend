ffbbaebf16a86e1212279537934b3886c915f13b
Katrin Chercas <katerinashepeneva@yandex.ru>
2022-11-26 13:08:45 +0300

    feat: create header user

--- oggetto/src/components/Header/Header.tsx
+++ oggetto/src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import { Container, Flex } from "@chakra-ui/react"
 import { Navigation } from "../Navigation/Navigation"
+import { HeaderUser } from "./HeaderUser"
 
 export const Header = () => {
     return (
         <Container variant={'header'}>
-            <Flex>Юзер</Flex>
+            <HeaderUser />
             <Navigation />
         </Container>
     )
