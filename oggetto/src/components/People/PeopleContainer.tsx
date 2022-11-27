6791613e0d3f654e745711a210b81a90ef3772ed
Katrin Chercas <katerinashepeneva@yandex.ru>
2022-11-27 06:54:50 +0300

    feat: filter by office

--- oggetto/src/components/People/PeopleContainer.tsx
+++ oggetto/src/components/People/PeopleContainer.tsx
@@ -1,14 +1,18 @@
 import { Container, Spinner } from "@chakra-ui/react"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
+import { User } from "../../interfaces/user.interface"
 import { getUsers } from "../../services/api/dataServices"
 import { PeopleAdvice } from "./PeopleAdvice"
 import { PeopleAround } from "./PeopleAround"
 import { PeopleAway } from "./PeopleAway"
 
 export const PeopleContainer = () => {
-    const [users, setUsers] = useState([])
+    const [users, setUsers] = useState<User[]>([])
+    const [usersAround, setUsersAround] = useState<User[]>([])
+    const [usersAway, setUsersAway] = useState<User[]>([])
     const [isLoading, setIsLoading] = useState(true)
+    const user: User = JSON.parse(localStorage.getItem("user") || '')
     const router = useRouter()
 
     const getData = async () => {
@@ -22,6 +26,11 @@ export const PeopleContainer = () => {
     }
 
     useEffect(() => {
+        setUsersAround(users.filter((item) => item.office === user.office))
+        setUsersAway(users.filter((item) => item.office !== user.office))
+    }, [users])
+
+    useEffect(() => {
         if (isLoading) getData()
     }, [])
 
@@ -30,8 +39,8 @@ export const PeopleContainer = () => {
             {(isLoading && !users.length) ? <Spinner /> :
                 <Container>
                     <PeopleAdvice goToPage={goToPage} users={users} />
-                    <PeopleAround goToPage={goToPage} users={users} />
-                    <PeopleAway goToPage={goToPage} users={users} />
+                    <PeopleAround goToPage={goToPage} users={usersAround} />
+                    <PeopleAway goToPage={goToPage} users={usersAway} />
                 </Container>
             }
         </Container>
