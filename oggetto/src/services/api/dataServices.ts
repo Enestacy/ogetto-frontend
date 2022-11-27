271fac3349f1f45d58cb1c6554607bdfe0c849d3
Katrin Chercas <katerinashepeneva@yandex.ru>
2022-11-27 11:14:19 +0300

    feat: add type

--- oggetto/src/services/api/dataServices.ts
+++ oggetto/src/services/api/dataServices.ts
@@ -2,6 +2,6 @@ import {get, patch,
 } from './baseRequests'
 
 export const getTags = () => get('tags')
-export const getTasks = (id: string) => get(`random-tasks/${id}`)
+export const getTasks = (id: string | string[] | undefined | null) => get(`random-tasks/${id}`)
 export const getUsers = () => get('users')
 export const patchAdviceUsers = (body: any) => patch('find-match', body)
